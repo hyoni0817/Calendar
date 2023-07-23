c31cf1715964cf61d4ce508f76fa8e7477a2580d
hyoni0817 <hyoni0817@naver.com>
2023-07-24 01:05:56 +0900

    Feat: 날짜 표현을 제외한 한 달의 각 주를 나타낼 수 있도록 구현

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -4,19 +4,27 @@ const Calendar = () => {
     const date = new Date();
     const dayOfTheWeek = ['일', '월', '화', '수', '목', '금', '토'];
     const lastDayOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const currentMonth = date.getMonth();
-
+    const currentYear = Number(date.getFullYear());
+    const currentMonth = Number(date.getMonth());
+    const numOfWeek = Math.floor(lastDayOfTheMonth[currentMonth] / 7) + (new Date(currentYear, currentMonth, 1).getDay() > 4 ? 2 : 1); // 1일이 금요일부터 시작되면 한 주를 더 해준다.
+    // 25, 26, 27, 28, 19, 30, 1
+    // 2, 3, 4, 5, 6, 7, 8
+    // 9, 10, 11, 12, 13, 14, 15,
+    // 16, 17, 18, 19, 20, 21, 22,
+    // 23, 24, 25, 26, 27, 28, 29,
+    // 30, 31, 1, 2, 3, 4, 5
     return (
         <div>
             <h1>{currentMonth+1}월</h1>
             <table>
                 <thead>
                     <tr>
-                        {dayOfTheWeek.map((day) => <th key="day">{day}</th>)}
+                        {dayOfTheWeek.map((day) => <th key={day}>{day}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    {
+                        React.Children.toArray(Array(numOfWeek).fill('').map((num, idx) => <tr>
                         <td>
                             1
                         </td>
@@ -38,7 +46,8 @@ const Calendar = () => {
                         <td>
                             7
                         </td>
-                    </tr>
+                    </tr>))
+                    }
                 </tbody>
             </table>
         </div>
